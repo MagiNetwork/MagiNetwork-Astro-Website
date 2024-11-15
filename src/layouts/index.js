997ca49a0a987c1e398a4f0a2213f73b2c3bc2bd
Mattia <162014917+0tia0@users.noreply.github.com>
2024-11-15 19:43:12 +0100

    removed old comments from layout javascript file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,11 @@
 const background = document.querySelector('.back');
 const dots = [];
-const numDotsX = 20; // Numero di pallini sull'asse X
-const numDotsY = 20; // Numero di pallini sull'asse Y
+const numDotsX = 20;
+const numDotsY = 20;
 
-// Dimensioni dello schermo
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
 
-// Spaziatura
 const spacingX = screenWidth / numDotsX;
 const spacingY = screenHeight / numDotsY;
 
@@ -16,33 +14,28 @@ for (let i = 0; i < numDotsX; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
     dot.style.left = `${i * spacingX}px`;
-    dot.style.top = `${125 + j * spacingY}px`; // Partiamo da 80px in giù
+    dot.style.top = `${125 + j * spacingY}px`;
     background.appendChild(dot);
     dots.push(dot);
   }
 }
 
-// Coordinate mouse (inizialmente al centro dello schermo)
 let mouseX = screenWidth / 2;
 let mouseY = screenHeight / 2;
 
-// Aggiorna le coordinate del mouse
 document.addEventListener('mousemove', (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 });
 
-// Effetto di animazione
 function animateDots() {
   dots.forEach((dot) => {
     const dotX = parseFloat(dot.style.left);
     const dotY = parseFloat(dot.style.top);
 
-    // Calcola lo spostamento in base alla distanza dal mouse
-    const deltaX = (mouseX - dotX) * 0.03; // Velocità di movimento
+    const deltaX = (mouseX - dotX) * 0.03; 
     const deltaY = (mouseY - dotY) * 0.03;
 
-    // Aggiorna la posizione del pallino
     dot.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
   });
 
